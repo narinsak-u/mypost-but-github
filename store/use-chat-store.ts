ace6110c1593d2477701b9db5a994643cc50e097
narinsak.u <nawongsak@gmail.com>
2026-05-15 22:58:11 +0700

    feat: add isCollapsed state to chat store

--- store/use-chat-store.ts
+++ store/use-chat-store.ts
@@ -2,17 +2,23 @@ import { create } from "zustand";
 
 interface ChatStore {
   isOpen: boolean;
+  isCollapsed: boolean; // New state
   currentConversationId: string | null;
   open: (conversationId?: string | null) => void;
   close: () => void;
+  toggleCollapse: () => void; // New action
+  setCollapsed: (val: boolean) => void; // New action
 }
 
 const useChatStore = create<ChatStore>((set) => ({
   isOpen: false,
+  isCollapsed: false,
   currentConversationId: null,
   open: (conversationId = null) =>
-    set({ isOpen: true, currentConversationId: conversationId }),
-  close: () => set({ isOpen: false, currentConversationId: null }),
+    set({ isOpen: true, isCollapsed: false, currentConversationId: conversationId }),
+  close: () => set({ isOpen: false, isCollapsed: false, currentConversationId: null }),
+  toggleCollapse: () => set((state) => ({ isCollapsed: !state.isCollapsed })),
+  setCollapsed: (val: boolean) => set({ isCollapsed: val }),
 }));
 
 export default useChatStore;

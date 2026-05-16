9da867a4cc8d74e20dd9ed29ae288309758cc717
narinsak.u <nawongsak@gmail.com>
2026-05-16 13:39:58 +0700

    fix: add github.com to next/image remote patterns
    Allows next/image to load fallback avatar URLs from github.com
    (e.g., https://github.com/shadcn.png used in UserAvatar)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    cacheComponents: true,
-}
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
